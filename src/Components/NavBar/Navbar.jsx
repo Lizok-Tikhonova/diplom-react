50709d6f69e032197183546ba26477a2ee0ff8da
Lizok-Tikhonova <tliza5092@gmail.com>
2023-10-19 21:02:30 +0300

    NavBar

---
+++ src/Components/NavBar/Navbar.jsx
@@ -0,0 +1,41 @@
+import styles from './NavBar.module.css'
+
+const NavBar = () => {
+    return ( 
+        <nav className={styles.nav}>
+            <div className='container'>
+                <div className={styles.nav_row}>
+                    <a href="" className={styles.logo}>
+                        Haircuts<span>&</span>Coloristics
+                    </a>
+                    <ul className={styles.nav_list}>
+                        <li className={styles.item}>
+                            <a href="" className={styles.link}>
+                               Главная
+                            </a>
+                        </li>
+                        <li className={styles.item}>
+                            <a href="" className={styles.link}>
+                               Услуги
+                            </a>
+                        </li>
+                        <li className={styles.item}>
+                            <a href="" className={styles.link}>
+                               Портфолио
+                            </a>
+                        </li>
+                        <li className={styles.item}>
+                            <a href="" className={styles.link}>
+                               Контакты
+                            </a>
+                        </li>
+                    </ul>
+                   
+                </div>
+                <hr className={styles.hr}></hr>
+            </div>
+        </nav>
+     );
+}
+ 
+export default NavBar;
