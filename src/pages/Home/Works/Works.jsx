69554c4c8e2a23b426e073c65011ab270993ab15
Lizok-Tikhonova <tliza5092@gmail.com>
2024-02-26 14:04:11 +0300

    правки

--- src/pages/Home/Works/Works.jsx
+++ src/pages/Home/Works/Works.jsx
@@ -1,31 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { arrmasters } from "../../../helpers/Masters"
 import style from "./Works.module.css"
-import work1 from "./work1.jpg"
-import work2 from "./work2.jpg"
-import work3 from "./work3.jpg"
-import work4 from "./work4.jpg"
-import work5 from "./work5.jpg"
-import work6 from "./work6.jpg"
-import work7 from "./work7.jpg"
-import work8 from "./work8.jpg"
+// import work1 from "./work1.jpg"
+// import work2 from "./work2.jpg"
+// import work3 from "./work3.jpg"
+// import work4 from "./work4.jpg"
+// import work5 from "./work5.jpg"
+// import work6 from "./work6.jpg"
+// import work7 from "./work7.jpg"
+// import work8 from "./work8.jpg"
 
-
+console.log(arrmasters[0].portfolio[1])
 const Works = ()=>{
     return(
         <section className={style.ourWorks}>
             <h2 className={style.title}>Наши работы</h2>
             <div className={style.wraperLink}>
-                <a href="#" className={style.link}>Перейти в портфолио</a>
+                <Link to={`/masters`} className={style.link}>Перейти к мастерам</Link>
             </div>
             
             <div className={style.works}>
-                <img src={work1} alt="" className={style.img} />
-                <img src={work2} alt="" className={style.img} />
-                <img src={work3} alt="" className={style.img} />
-                <img src={work4} alt="" className={style.img} />
-                <img src={work5} alt="" className={style.img} />
-                <img src={work6} alt="" className={style.img} />
-                <img src={work7} alt="" className={style.img} />
-                <img src={work8} alt="" className={style.img} />
+                <img src={arrmasters[0].portfolio[0]} alt="work1" className={style.img} />
+                <img src={arrmasters[0].portfolio[1]} alt="work2" className={style.img} />
+                <img src={arrmasters[0].portfolio[2]} alt="work3" className={style.img} />
+                <img src={arrmasters[0].portfolio[3]} alt="work4" className={style.img} />
+                <img src={arrmasters[0].portfolio[4]} alt="work5" className={style.img} />
+                <img src={arrmasters[0].portfolio[5]} alt="work6" className={style.img} />
+                <img src={arrmasters[0].portfolio[6]} alt="work7" className={style.img} />
+                <img src={arrmasters[0].portfolio[7]} alt="work8" className={style.img} />
             </div>
         </section>
     )
